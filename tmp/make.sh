64f2e189148f19a733fda3164ebfbbe03ca901e0
MacroUniverse <270174408@qq.com>
2023-07-09 12:33:02 -0500

    update

--- tmp/make.sh
+++
@@ -1,4 +0,0 @@
-g++ -c -std=c++17 -Wall -I/home/addis/Qt/6.4.2/gcc_64/include -I/home/addis/Qt/6.4.2/gcc_64/include/{QtWidgets,QtGui} -fPIC main.cpp main_window.cpp
-
-g++ main.o main_window.o -o qt_example -L/home/addis/Qt/Tools/QtDesignStudio/qt5_design_studio_reduced_version/lib/ -lQt5Core -lQt5Gui -lQt5Widgets
-
